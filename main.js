29c79ac727e7b123baac7d915f5df6b964fbb09f
jlund <dev@jakoblundoe.com>
2025-01-05 15:19:35 +0100

    Make the grid generate button replace former grid with new grid

--- main.js
+++ main.js
@@ -8,32 +8,39 @@ addEventListener('DOMContentLoaded', () => {
         let gridDensity = window.prompt("Specify grid density by entering number of squares per row:")
         cellNumber = calcCellAmount(gridDensity);
         console.log(cellNumber);
+        gridContainer.replaceChildren();
+        generateNewGrid();
     })
 
-    for (let i = 1; i <= cellNumber; i++) {
-        const gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
-        gridCell.setAttribute(`cell-id`, String(i));
-        gridContainer.appendChild(gridCell);
-    }
+    generateNewGrid()
 
-    for (let i = 1; i <= cellNumber; i++) {
-        const cellID = document.querySelector(`[cell-id="${i}"]`);
-        console.log(cellID);
-        cellID.addEventListener('mouseover', () => {
-            if (cellID.classList.contains('transition-out')) {
-                cellID.classList.replace('transition-out', 'transition-in');
-            } else {
-                cellID.classList.add('transition-in');
-            }
-            cellID.classList.add('pixel-hover');
-        })
-        cellID.addEventListener('mouseout', () => {
-            cellID.classList.replace('transition-in', 'transition-out');
-            cellID.classList.remove('pixel-hover');
-        })
+    function generateNewGrid() {
+        for (let i = 1; i <= cellNumber; i++) {
+            const gridCell = document.createElement('div');
+            gridCell.classList.add('grid-cell');
+            gridCell.setAttribute(`cell-id`, String(i));
+            gridContainer.appendChild(gridCell);
+        }
+
+        for (let i = 1; i <= cellNumber; i++) {
+            const cellID = document.querySelector(`[cell-id="${i}"]`);
+            console.log(cellID);
+            cellID.addEventListener('mouseover', () => {
+                if (cellID.classList.contains('transition-out')) {
+                    cellID.classList.replace('transition-out', 'transition-in');
+                } else {
+                    cellID.classList.add('transition-in');
+                }
+                cellID.classList.add('pixel-hover');
+            })
+            cellID.addEventListener('mouseout', () => {
+                cellID.classList.replace('transition-in', 'transition-out');
+                cellID.classList.remove('pixel-hover');
+            })
+        }
     }
 })
+
 
 function calcCellAmount (input) {
     return input*input;
