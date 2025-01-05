eafa5d111865b145c02b1935c4c7653cb9f390e7
jlund <dev@jakoblundoe.com>
2025-01-05 14:14:21 +0100

    Create hover effect on grid cells

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ addEventListener('DOMContentLoaded', () => {
 
     for (let i = 1; i <= 256; i++) {
         const gridCell = document.createElement('div');
-        gridCell.classList.add('gridCell');
+        gridCell.classList.add('grid-cell');
+        gridCell.setAttribute(`cell-id`, String(i));
         gridContainer.appendChild(gridCell);
+    }
+
+    for (let i = 1; i <= 256; i++) {
+        const cellID = document.querySelector(`[cell-id="${i}"]`);
+        console.log(cellID);
+        cellID.addEventListener('mouseover', () => {
+            cellID.classList.add('pixel-hover');
+        })
+        cellID.addEventListener('mouseout', () => {
+            cellID.classList.remove('pixel-hover');
+        })
     }
 })
