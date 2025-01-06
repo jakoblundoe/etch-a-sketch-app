67f2b1d86cb1073c4ff0900f7bdc484870ae288e
jlund <dev@jakoblundoe.com>
2025-01-06 08:36:10 +0100

    Add color palette to generated hover + fade in/out

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ addEventListener('DOMContentLoaded', () => {
     })
 
     generateNewGrid(Math.sqrt(cellNumber));
+    generateColor();
 
     function generateNewGrid(rowSize) {
         console.log(rowSize);
-
 
         // GRID CREATION
         for (let i = 1; i <= cellNumber; i++) {
@@ -35,22 +35,36 @@ addEventListener('DOMContentLoaded', () => {
         //HOVER FEATURE
         for (let i = 1; i <= cellNumber; i++) {
             const cellID = document.querySelector(`[cell-id="${i}"]`);
+            const colorCode = generateColor();
+
             cellID.addEventListener('mouseover', () => {
                 if (cellID.classList.contains('transition-out')) {
                     cellID.classList.replace('transition-out', 'transition-in');
+                    cellID.style['background-color'] = colorCode;
                 } else {
                     cellID.classList.add('transition-in');
                 }
-                cellID.classList.add('pixel-hover');
+                cellID.style['background-color'] = colorCode;
             })
             cellID.addEventListener('mouseout', () => {
                 cellID.classList.replace('transition-in', 'transition-out');
-                cellID.classList.remove('pixel-hover');
+                cellID.style.removeProperty('background-color');
             })
         }
     }
 })
 
+// GENERATOR COLOR FROM PREDEFINED COLOR PALETTE
+function generateColor () {
+    const colorPalette = [
+        '#2A8C82',
+        '#41BFB3',
+        '#9BF2EA',
+        '#275950',
+        '#260101'
+    ]
+    return colorPalette[randomInt(colorPalette.length, 0)];
+}
 
 function calcCellAmount (input) {
     return input*input;
@@ -58,4 +72,8 @@ function calcCellAmount (input) {
 
 function calcCellWidth (input) {
     return 100/input;
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
